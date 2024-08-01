8dab2e80dc60aae431fd9e06ac295c36535da3d8
siniourz <kazemisina45@gmail.com>
2024-08-01 23:15:24 +0300

    final pert until the video of yt.

---
+++ leading.js
@@ -0,0 +1,28 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        height: '600',
+        width: '1015',
+        videoId: 'SoUZ3wovZAs',
+        events: {
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerStateChange(event) {
+    if (event.data == YT.PlayerState.PLAYING) {
+        document.querySelectorAll('.topic-h4, .topic-h1, .topic-h3, .topic-h4-2, .button-startnow-div').forEach(function(element) {
+            element.classList.add('hidden');
+        });
+    } else if (event.data == YT.PlayerState.PAUSED || event.data == YT.PlayerState.ENDED) {
+        document.querySelectorAll('.hidden').forEach(function(element) {
+            element.classList.remove('hidden');
+        });
+    }
+}
